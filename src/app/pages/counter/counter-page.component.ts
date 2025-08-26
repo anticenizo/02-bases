431b730ba1c89d2e72153f263fc8d24ac128323e
Raul del Hoyo Padilla <raul.delhoyo@trijazac.gob.mx>
2025-08-26 12:49:16 -0600

    Signals-exercice

--- src/app/pages/counter/counter-page.component.ts
+++ src/app/pages/counter/counter-page.component.ts
@@ -1,17 +1,40 @@
 
-import { Component } from '@angular/core';
+import { Component, provideZoneChangeDetection, signal } from '@angular/core';
 
 @Component({
-  template: `
-    <h1>Counter: {{ counter }}</h1>
-    <button (click)="increaseBy(1)">+1</button>
-    `,
+  //template: `
+  //  <h1>Counter: {{ counter }}</h1>
+  //  <button (click)="increaseBy(1)">+1</button>
+  //  `,
+  templateUrl: './counter-page.component.html',
+  styles: [
+    `
+      button{
+        padding: 5px;
+        margin: 5px 10px;
+        width: 75px;
+      }
+    `
+  ]
 })
 export class CounterPageComponent{
 
   counter = 10;
+  counterSignal = signal(10);
 
   increaseBy(value: number){
     this.counter += value;
+    this.counterSignal.update( current => current + value);
+  }
+
+  decreaseBy(value: number){
+    this.counter -= value;
+    this.counterSignal.update( current => current - value);
+  }
+
+  reset(){
+    this.counter = 0;
+    this.counterSignal.set(0);
+
   }
 }
