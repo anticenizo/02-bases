8240651a69d7ed1932877d802c14fb3831aee732
Raul del Hoyo Padilla <raul.delhoyo@trijazac.gob.mx>
2025-08-26 19:49:01 -0600

    hero1

--- src/app/pages/hero/hero-page.component.ts
+++ src/app/pages/hero/hero-page.component.ts
@@ -5,32 +5,30 @@ import { UpperCasePipe } from '@angular/common';
 @Component({
   selector: 'app-hero-page',
   templateUrl: './hero-page.component.html',
-  // styleUrls: ['./hero-page.component.css'],
-  imports: [UpperCasePipe],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 
 
 export class HeroPageComponent {
 
-  nameSignal = signal('Ironman');
-  ageSignal = signal(45);
+  name = signal('Ironman');
+  age = signal(45);
 
-  getHeroDescription(name: Signal<string>, age: Signal<number>): string{
-    return `Yo soy ${name()} y tengo ${age()} años`;
+  getHeroDescription(): string{
+    return `Yo soy ${this.name()} y tengo ${this.age()} años`;
   }
 
   changeHero(): void{
-    this.nameSignal.update(() => 'Spiderman');
-    this.ageSignal.update(() => 22);
+    this.name.set('Spiderman');
+    this.age.set(22);
   }
 
   resetForm(): void{
-    this.nameSignal.update(() => 'Ironman');
-    this.ageSignal.update(() => 45);
+    this.name.set('Ironman');
+    this.age.set(45);
   }
 
   changeAge(): void{
-    this.ageSignal.update(() => 60);
+    this.age.set(60);
   }
 }
