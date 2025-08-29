8fff2f84147e545e5b5b6f379d99b2b83686a9b7
Raul del Hoyo Padilla <raul.delhoyo@trijazac.gob.mx>
2025-08-28 19:25:43 -0600

    @if

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -10,9 +10,8 @@ interface Character {
 
 
 @Component({  selector: 'app-dragonball-page',
-  templateUrl: './dragonball-page.component.html',
-  imports: [NgClass],
 
+  templateUrl: './dragonball-page.component.html',
 
 })
 export class DragonballPageComponent {
@@ -20,7 +19,9 @@ export class DragonballPageComponent {
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001},
     { id: 2, name: 'Vegeta', power: 8000},
-    { id: 3, name: 'Piccolo', power: 3000},]);
+    { id: 3, name: 'Piccolo', power: 3000},
+    { id: 4, name: 'Yamcha', power: 500},
+  ]);
 
     // powerClasses = computed(() => {
     //   return {
